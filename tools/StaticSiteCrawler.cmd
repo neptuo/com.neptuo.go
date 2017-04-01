a358549ca4c4bc1fde5ab77579e8e5d3f6fe87ac
Marek Fišera <mara@neptuo.com>
2017-04-01 23:57:57 +0200

    Trying to generate static site outside of the checkout.

--- tools/StaticSiteCrawler.cmd
+++ tools/StaticSiteCrawler.cmd
@@ -1,8 +1,8 @@
 Tools\StaticSiteCrawler.exe http://localhost:58537/ docs /
 
-xcopy src\WebSite\Content\Images docs\Content\Images /E /R /Y
-xcopy src\WebSite\Content\bootstrap.min.css docs\Content /R /Y
-xcopy src\WebSite\Content\bootstrap-theme.min.css docs\Content /R /Y
-xcopy src\WebSite\Content\web.css docs\Content /R /Y
-xcopy src\WebSite\fonts docs\fonts /E /R /Y
-xcopy src\WebSite\Scripts docs\Scripts /E /R /Y
+xcopy src\WebSite\Content\Images ..\output\docs\Content\Images /E /R /Y
+xcopy src\WebSite\Content\bootstrap.min.css ..\output\docs\Content /R /Y
+xcopy src\WebSite\Content\bootstrap-theme.min.css ..\output\docs\Content /R /Y
+xcopy src\WebSite\Content\web.css ..\output\docs\Content /R /Y
+xcopy src\WebSite\fonts ..\output\docs\fonts /E /R /Y
+xcopy src\WebSite\Scripts ..\output\docs\Scripts /E /R /Y
