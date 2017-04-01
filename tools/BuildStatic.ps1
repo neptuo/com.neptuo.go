645967b088389e55c538d7323a41232584aa5623
Marek Fišera <mara@neptuo.com>
2017-04-01 23:36:42 +0200

    Add debug message to build script. Fix paths in StaticSiteCrawler.

--- tools/BuildStatic.ps1
+++ tools/BuildStatic.ps1
@@ -3,7 +3,11 @@ param([string]$buildFolder)
 $path = "$($buildFolder)\src\WebSite"
 $port = 58537
 
+Write-Host "Running IIS Express from '$($path)'."
 Start-Process "C:\Program Files (x86)\IIS Express\iisexpress.exe" -NoNewWindow -ArgumentList "/path:$($path) /port:$($port)"
+
+Write-Host "Running StaticSiteCrawler."
 Start-Process "$($buildFolder)\Tools\StaticSiteCrawler.cmd"
 
+Write-Host "Stopping IIS Express."
 Stop-Process -Name iisexpress
--- tools/StaticSiteCrawler.cmd
+++ tools/StaticSiteCrawler.cmd
@@ -1,8 +1,8 @@
-StaticSiteCrawler.exe http://localhost:58537/ ..\docs / /404.html
+Tools\StaticSiteCrawler.exe http://localhost:58537/ docs / /404.html
 
-xcopy ..\src\com.neptuo.go\Content\Images ..\docs\Content\Images /E /R /Y
-xcopy ..\src\com.neptuo.go\Content\bootstrap.min.css ..\docs\Content /R /Y
-xcopy ..\src\com.neptuo.go\Content\bootstrap-theme.min.css ..\docs\Content /R /Y
-xcopy ..\src\com.neptuo.go\Content\web.css ..\docs\Content /R /Y
-xcopy ..\src\com.neptuo.go\fonts ..\docs\fonts /E /R /Y
-xcopy ..\src\com.neptuo.go\Scripts ..\docs\Scripts /E /R /Y
+xcopy src\com.neptuo.go\Content\Images docs\Content\Images /E /R /Y
+xcopy src\com.neptuo.go\Content\bootstrap.min.css docs\Content /R /Y
+xcopy src\com.neptuo.go\Content\bootstrap-theme.min.css docs\Content /R /Y
+xcopy src\com.neptuo.go\Content\web.css docs\Content /R /Y
+xcopy src\com.neptuo.go\fonts docs\fonts /E /R /Y
+xcopy src\com.neptuo.go\Scripts docs\Scripts /E /R /Y
