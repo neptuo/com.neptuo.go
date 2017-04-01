3da8d36e161cfbbba514d3cd54392c8e55459f39
Marek Fišera <mara@neptuo.com>
2017-04-01 22:44:43 +0200

    Add static generation tools.

---
+++ tools/StaticSiteCrawler.cmd
@@ -0,0 +1,10 @@
+StaticSiteCrawler.exe http://localhost:7287/ ..\docs / /404.html
+
+xcopy ..\src\com.neptuo.go\Content\Images ..\docs\Content\Images /E /R /Y
+xcopy ..\src\com.neptuo.go\Content\bootstrap.min.css ..\docs\Content /R /Y
+xcopy ..\src\com.neptuo.go\Content\bootstrap-theme.min.css ..\docs\Content /R /Y
+xcopy ..\src\com.neptuo.go\Content\web.css ..\docs\Content /R /Y
+xcopy ..\src\com.neptuo.go\fonts ..\docs\fonts /E /R /Y
+xcopy ..\src\com.neptuo.go\Scripts ..\docs\Scripts /E /R /Y
+
+pause
