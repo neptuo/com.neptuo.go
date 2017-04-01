ec2a4cb7c9abaaf33dee4ba4a4f3c426a6ec1495
Marek Fišera <mara@neptuo.com>
2017-04-02 00:19:17 +0200

    Move static site to gh-pages branch.

--- tools/StaticSiteCrawler.cmd
+++ tools/StaticSiteCrawler.cmd
@@ -1,8 +1,8 @@
-Tools\StaticSiteCrawler.exe http://localhost:58537/ ..\output\docs /
+Tools\StaticSiteCrawler.exe http://localhost:58537/ ..\output /
 
-xcopy src\WebSite\Content\Images ..\output\docs\Content\Images /E /R /Y
-xcopy src\WebSite\Content\bootstrap.min.css ..\output\docs\Content /R /Y
-xcopy src\WebSite\Content\bootstrap-theme.min.css ..\output\docs\Content /R /Y
-xcopy src\WebSite\Content\web.css ..\output\docs\Content /R /Y
-xcopy src\WebSite\fonts ..\output\docs\fonts /E /R /Y
-xcopy src\WebSite\Scripts ..\output\docs\Scripts /E /R /Y
+xcopy src\WebSite\Content\Images ..\output\Content\Images /E /R /Y
+xcopy src\WebSite\Content\bootstrap.min.css ..\output\Content /R /Y
+xcopy src\WebSite\Content\bootstrap-theme.min.css ..\output\Content /R /Y
+xcopy src\WebSite\Content\web.css ..\output\Content /R /Y
+xcopy src\WebSite\fonts ..\output\fonts /E /R /Y
+xcopy src\WebSite\Scripts ..\output\Scripts /E /R /Y
