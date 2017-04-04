b2c956af783c9750a6eedfafc42438537388df11
Marek Fišera <mara@neptuo.com>
2017-04-04 20:25:42 +0200

    Remove /docs folder. Using directly StaticSiteCrawler.exe.

--- tools/BuildStatic.ps1
+++ tools/BuildStatic.ps1
@@ -2,16 +2,17 @@ param([string]$buildFolder)
 
 $path = "$($buildFolder)\src\WebSite"
 $port = 58537
-$delay = 10000
+$delay = 3000
 
 Write-Host "Running IIS Express from '$($path)'."
 Start-Process "C:\Program Files (x86)\IIS Express\iisexpress.exe" -NoNewWindow -ArgumentList "/path:$($path) /port:$($port)"
 
-Write-Host "Running StaticSiteCrawler."
-Start-Process "$($buildFolder)\Tools\StaticSiteCrawler.cmd" -Wait
-
 Write-Host "Waiting $($delay)."
 Start-Sleep -Milliseconds $delay
+
+Write-Host "Running StaticSiteCrawler."
+#Start-Process "$($buildFolder)\Tools\StaticSiteCrawler.cmd" -Wait
+.\Tools\StaticSiteCrawler.exe http://localhost:$($port)/ ..\output /
 
 Write-Host "Content of output:"
 $items = Get-ChildItem -Path ..\output
