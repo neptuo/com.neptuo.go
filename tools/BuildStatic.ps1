d91836439ca2fa50ad057a6fc5c0e6adc24f2380
Marek Fišera <mara@neptuo.com>
2017-04-04 21:20:58 +0200

    Trying to copy all static assets.

--- tools/BuildStatic.ps1
+++ tools/BuildStatic.ps1
@@ -3,17 +3,23 @@ param([string]$buildFolder)
 $outputPath = "..\output";
 $sitePath = "$($buildFolder)\src\WebSite"
 $port = 58537
-$delay = 10000
+$delay = 3000
 
 Write-Host "Running IIS Express from '$($sitePath)' at '$($port)'."
 $iis = Start-Process "C:\Program Files (x86)\IIS Express\iisexpress.exe" -NoNewWindow -ArgumentList "/path:$($sitePath) /port:$($port)"
 
-Write-Host "Running StaticSiteCrawler."
-Start-Process "$($buildFolder)\Tools\StaticSiteCrawler.cmd"
-#.\Tools\StaticSiteCrawler.exe http://localhost:$($port)/ $outputPath /
-
 Write-Host "Waiting $($delay)."
 Start-Sleep -Milliseconds $delay
+
+Write-Host "Running StaticSiteCrawler."
+.\Tools\StaticSiteCrawler.exe http://localhost:$($port)/ $outputPath /
+
+Copy-Item "$($sitePath)\Content\Images" "$($outputPath)\Content\Images" -Force -Recurse
+Copy-Item "$($sitePath)\Content\bootstrap.min.css" "$($outputPath)\Content" -Force -Recurse
+Copy-Item "$($sitePath)\Content\bootstrap-theme.min.css" "$($outputPath)\Content" -Force -Recurse
+Copy-Item "$($sitePath)\Content\web.css" "$($outputPath)\Content" -Force -Recurse
+Copy-Item "$($sitePath)\fonts" "$($outputPath)\fonts" -Force -Recurse
+Copy-Item "$($sitePath)\Scripts" "$($outputPath)\Scripts" -Force -Recurse
 
 Write-Host "Content of output:"
 $items = Get-ChildItem -Path $outputPath
