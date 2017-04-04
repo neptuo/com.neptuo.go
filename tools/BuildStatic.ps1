79e964add5e675dd44c3debfd64b04a92f9f5535
Marek Fišera <mara@neptuo.com>
2017-04-04 21:16:17 +0200

    Using StaticSiteCrawler.cmd.

--- tools/BuildStatic.ps1
+++ tools/BuildStatic.ps1
@@ -3,25 +3,17 @@ param([string]$buildFolder)
 $outputPath = "..\output";
 $sitePath = "$($buildFolder)\src\WebSite"
 $port = 58537
-$delay = 5000
+$delay = 10000
 
 Write-Host "Running IIS Express from '$($sitePath)' at '$($port)'."
 $iis = Start-Process "C:\Program Files (x86)\IIS Express\iisexpress.exe" -NoNewWindow -ArgumentList "/path:$($sitePath) /port:$($port)"
 
+Write-Host "Running StaticSiteCrawler."
+Start-Process "$($buildFolder)\Tools\StaticSiteCrawler.cmd"
+#.\Tools\StaticSiteCrawler.exe http://localhost:$($port)/ $outputPath /
+
 Write-Host "Waiting $($delay)."
 Start-Sleep -Milliseconds $delay
-
-If ($iis.HasExited)
-{
-    Write-Host "IIS has exited";
-}
-else {
-    Invoke-WebRequest "http://localhost:$($port)/";
-}
-
-Write-Host "Running StaticSiteCrawler."
-#Start-Process "$($buildFolder)\Tools\StaticSiteCrawler.cmd" -Wait
-.\Tools\StaticSiteCrawler.exe http://localhost:$($port)/ $outputPath /
 
 Write-Host "Content of output:"
 $items = Get-ChildItem -Path $outputPath
