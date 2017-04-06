43405ec9ac682e3c3e5b7e88ab487588aa351ed1
Marek Fišera <mara@neptuo.com>
2017-04-06 18:48:08 +0200

    Basic support for displaying deprecated links.

---
+++ src/WebSite/_LinkModelExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Neptuo;
+using Neptuo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Neptuo
+{
+    public static class _LinkModelExtensions
+    {
+        public static string CssClassIfDeprecated(this LinkModel model)
+        {
+            Ensure.NotNull(model, "model");
+            if (model.IsDeprecated)
+                return "deprecated";
+
+            return null;
+        }
+    }
+}
